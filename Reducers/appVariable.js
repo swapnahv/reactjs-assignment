c199fca7098c0ea605888441940fd4543c195afa
swapnahv <46479037+swapnahv@users.noreply.github.com>
2019-02-03 12:48:55 +0530

    updating reducer

--- Reducers/appVariable.js
+++ Reducers/appVariable.js
@@ -1,5 +1,14 @@
-export default function(){
-	return({
-		loggedIn:true
-	})
-}
+const initialState = {
+	loggedIn : false
+};
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case "LOGGING":
+			console.log(action.type);
+			return {
+				loggedIn: action.payload
+			};
+	  	default:
+			return state;
+	}
+  };
